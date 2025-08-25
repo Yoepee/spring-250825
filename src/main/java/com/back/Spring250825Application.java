1485d90b4f46dc75298d648799ab220a6a0c1f2c
Yoepee <kdy2975@naver.com>
2025-08-25 15:44:45 +0900

    init

---
+++ src/main/java/com/back/Spring250825Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring250825Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring250825Application.class, args);
+    }
+
+}
