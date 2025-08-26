25ba3a9b90893c4b4ec33276f76da0c71f898b3c
Yoepee <kdy2975@naver.com>
2025-08-26 09:22:11 +0900

    기반코드 setup

---
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -0,0 +1,7 @@
+package com.back.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PostController {
+}
---
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.back.global.jpa.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
