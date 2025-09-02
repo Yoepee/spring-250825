6d65f1b210fa007bc959bbee7f6aadb5f44c90bd
Yoepee <kdy2975@naver.com>
2025-09-02 09:57:43 +0900

    회원가입, 로그인 폼 구현

--- src/main/java/com/back/domain/member/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/MemberController.java
@@ -5,14 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("join")
+    @GetMapping("/login")
     public String join(Model model) {
-        return "member/join";
+        return "member/member/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegister() {
+        return "member/member/register";
     }
 }
